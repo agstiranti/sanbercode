e0ad88593f42dda894fd040bb80e071871af2870
agstiranti <tifani.agustiranti@gmail.com>
2026-01-11 22:27:44 +0700

    Update code terbaru

--- cypress/e2e/bootcamp_sanber.cy.js
+++ cypress/e2e/bootcamp_sanber.cy.js
@@ -4,6 +4,8 @@ describe ('OrangeHRM Login Feature Automation', () => {
         cy.get('input[placeholder="Username"]').type('Admin')
         cy.get('input[placeholder="Password"]').type('admin123')
         cy.get('button[type="submit"]').click()
+        cy.url().should('include', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
     })
 })
 
@@ -13,6 +15,7 @@ describe ('OrangeHRM Login Feature Automation TC02', () => {
         cy.get('input[placeholder="Username"]').type('AdmiNNN')
         cy.get('input[placeholder="Password"]').type('admin1234')
         cy.get('button[type="submit"]').click()
+        cy.url().should('include', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     })
 })
 
@@ -22,31 +25,34 @@ describe ('OrangeHRM Login Feature Automation TC03', () => {
         cy.get('input[placeholder="Username"]').type('AdmiNNN')
         cy.get('input[placeholder="Password"]').type('admin123456')
         cy.get('button[type="submit"]').click()
+        cy.url().should('include', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     })
 })
 
 describe ('OrangeHRM Login Feature Automation TC04', () => {
     it('TC_LOGIN_04: Should show "Required" error when username is empty', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com')
-        cy.get('input[placeholder="Username"]').type('null')
+        cy.get('input[placeholder="Username"]').clear('')
         cy.get('input[placeholder="Password"]').type('admin1234')
         cy.get('button[type="submit"]').click()
+        cy.url().should('include', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     })
 
     describe ('OrangeHRM Login Feature Automation 05', () => {
         it('TC_LOGIN_05: Should show "Required" error when password is empty', () => {
             cy.visit('https://opensource-demo.orangehrmlive.com')
             cy.get('input[placeholder="Username"]').type('AdmiNNN')
-            cy.get('input[placeholder="Password"]').type('null')
+            cy.get('input[placeholder="Password"]').clear('')
             cy.get('button[type="submit"]').click()
         })
     })
     describe ('OrangeHRM Login Feature Automation 06', () => {
         it('TC_LOGIN_06: Should show "Required" error when both fields are empty', () => {
             cy.visit('https://opensource-demo.orangehrmlive.com')
-            cy.get('input[placeholder="Username"]').type('null')
-            cy.get('input[placeholder="Password"]').type('null')
+            cy.get('input[placeholder="Username"]').clear('')
+            cy.get('input[placeholder="Password"]').clear('')
             cy.get('button[type="submit"]').click()
+            cy.url().should('include', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         })
     })
 })
